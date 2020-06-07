ac4aaa3085ca6a43be097a7b16ffa0dd95258acd
UgurSari99 <66373170+UgurSari99@users.noreply.github.com>
2020-06-07 22:03:24 +0200

    Add files via upload

---
+++ multiply.py
@@ -0,0 +1,9 @@
+def carp():
+    print('Carpma Yapmak Icin Birinci Sayiyi Giriniz:')
+    num1= int (input())
+    print('Carpma Yapmak Icin Ikinci Sayiyi Giriniz:')
+    num2= int (input())
+    print('Islem Sonucu:')
+    print(num1*num2)
+
+carp()
